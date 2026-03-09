9bc20c1c3be465dbcef2803270ae635bffe66bd9
iwe-coumou <iweynys@live.nl>
2026-03-09 23:52:50 +0100

    added calculator and pkg dirs with files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import argparse
 
 load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
@@ -7,8 +8,26 @@ if api_key is None:
     raise RuntimeError("GEMINI_API_KEY not retreival failed.")
 
 from google import genai
+from google.genai import types
 
 client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-print(response.text)
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
+# Now we can access `args.user_prompt`
+
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
+
+if response.usage_metadata is None:
+    raise RuntimeError("No response usage_metadate available")
+
+if args.verbose:
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+print(f"{response.text}")
